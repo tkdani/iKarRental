d8c8081acbe4e4610b2c91b756c4a0cefea426bc
danoo33 <dani.takacs2003@gmail.com>
2025-01-03 14:55:10 +0100

    car labels

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+// Read the JSON file
+$carsJson = file_get_contents('cars.json');
+
+// Decode the JSON data into a PHP array
+$cars = json_decode($carsJson, true);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -58,7 +65,27 @@
         </div>
     </div>
     <div class="content">
-
+        <?php if (!empty($cars)): ?>
+            <?php foreach ($cars as $car): ?>
+                <div class="carContainer">
+                    <div class="carImgContainer">
+                        <img class="carImg" src="<?php echo htmlspecialchars($car['image']); ?>">
+                        <span><?php echo $car['daily_price_huf'] . ' Ft' ?></span>
+                    </div>
+                    <div class="carInfo">
+                        <div>
+                            <p class="carBrand"><?php echo $car['brand'] ?> <b><?php echo $car['model']; ?></b></p>
+                            <p class="carPassengers"><?php echo $car['passengers'] . ' férőhely - ' . $car['transmission']; ?></p>
+                        </div>
+                        <div class="registerBtn carBtn">
+                            <a class="register" href="#">Foglalás</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <p>Nincsenek elérhető autók.</p>
+        <?php endif; ?>
     </div>
 
 
